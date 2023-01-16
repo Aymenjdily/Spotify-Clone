accd521d035661040638497292c131f61694a698
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-01-17 00:21:18 +0100

    second push

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,7 +2,6 @@ import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 const Discover = () => {
-  console.log(genres);
   const genreTitle = 'Pop';
   return (
     <div className="flex flex-col">
